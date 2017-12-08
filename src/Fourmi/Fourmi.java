8081bf789ae0c66b3c83026cc7ab8af2ac404c71
florian <florian@172.28.143.115>
2017-12-08 15:30:16 +0100

    ajout et debut de la partit de rapport des fourmi

--- src/Fourmi/Fourmi.java
+++ src/Fourmi/Fourmi.java
@@ -3,26 +3,33 @@ package Fourmi;
 import Fourmi.EtatDev.EtatDev;
 import Fourmi.EtatDev.Oeuf;
 import Fourmiliere.Fourmiliere;
+import suivi.Report;
+import suivi.Trace;
 
-public class Fourmi {
-  EtatDev etatDev;
-  Fourmiliere lafourmilliere;
-  
-  public Fourmi(Fourmiliere maFourmilliere) {
-	  lafourmilliere=maFourmilliere;
-	  etatDev = new Oeuf();
-	  cycle(this);
-  }
+public class Fourmi implements Trace {
+	EtatDev etatDev;
+	Fourmiliere lafourmilliere;
 
-  public void cycle(Fourmi maFourmis){
-    etatDev.cycle(maFourmis);
-  }
-  
-  public EtatDev getetatDev() {
-	  return etatDev;
-  }
-  
-  public void setetatDev(EtatDev etat) {
-	  etatDev=etat;
-  }
+	public Fourmi(Fourmiliere maFourmilliere) {
+		lafourmilliere = maFourmilliere;
+		etatDev = new Oeuf();
+		cycle(this);
+	}
+
+	public void cycle(Fourmi maFourmis) {
+		etatDev.cycle(maFourmis);
+	}
+
+	public EtatDev getetatDev() {
+		return etatDev;
+	}
+
+	public void setetatDev(EtatDev etat) {
+		etatDev = etat;
+	}
+
+	@Override
+	public void trace(Report r) {
+		
+	}
 }
---
+++ src/suivi/CountFourmi.java
@@ -0,0 +1,11 @@
+package suivi;
+
+import Fourmi.Fourmi;
+
+class CountFourmi extends Report {
+	int compteur = 0;
+	void traceForFourmi(Fourmi f ) {
+		compteur++;
+	}
+
+}
