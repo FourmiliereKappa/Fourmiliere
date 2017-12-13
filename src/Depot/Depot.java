cf69ca76890db2d974c8117210039fbd0d19bfa7
bossertsebastien <seb298600@gmail.com>
2017-12-13 20:33:06 +0100

    Add files via upload

---
+++ src/Depot/Depot.java
@@ -0,0 +1,59 @@
+package Depot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import drawing.IMovableDrawable;
+import monde.Terrain;
+import shapeGiver.Dessinable;
+import shapeGiver.Dessineur;
+import shapeGiver.SkinType1;
+
+public class Depot implements Dessinable {
+	
+	int x,y;
+	List<Cadavre> ListeCadavre;
+	IMovableDrawable skin;
+	
+	public Depot(int x,int y) {
+		this.x=x;
+		this.y=y;
+		ListeCadavre = new ArrayList<Cadavre>();
+		Dessineur dessineur = new SkinType1();
+	    skin = accept(dessineur);
+	    
+	    Terrain.addDessinable(this);
+	}
+	
+	public void ajoutCadavre(Cadavre nvCadavre) {
+		getListeCadavre().add(nvCadavre);
+	}
+
+
+	@Override
+	public IMovableDrawable accept(Dessineur dessineur) {
+	  return dessineur.dessine(this);
+	}
+
+	
+	@Override
+	public IMovableDrawable getSkin() {
+	  return skin;
+	}
+
+
+	@Override
+	public int getX() {
+		return x;
+	}
+
+
+	@Override
+	public int getY() {
+		return y;
+	}
+	
+	public List<Cadavre> getListeCadavre() {
+		return ListeCadavre;
+	}
+}
