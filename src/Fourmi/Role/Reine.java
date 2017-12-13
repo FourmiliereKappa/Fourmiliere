f30f54414004928c0626e8b5dc46503be7bcfe32
bossertsebastien <seb298600@gmail.com>
2017-12-13 20:33:46 +0100

    Add files via upload

---
+++ src/Fourmi/Role/Reine.java
@@ -0,0 +1,55 @@
+
+package Fourmi.Role;
+
+import java.util.Random;
+
+import Fourmi.Fourmi;
+import Fourmi.EtatDev.Adulte;
+import Fourmiliere.Fourmiliere;
+import monde.Terrain;
+
+public class Reine extends Role {
+	
+  Adulte refAdulte;
+
+  int compteuravantmanger = 100;
+
+  Fourmiliere maFourmiliere;
+  
+  int dureevieReine = (int) (new Random().nextInt()*6+4) * 105120;  // nombre d'annee de vie de la reine  
+  // * 288 nb cycle jour * 365 nb jour année
+  
+  public Reine(Terrain monTerrain) {
+	 
+	 maFourmiliere = new Fourmiliere(monTerrain);
+	 
+	 Fourmi maFourmi = new Fourmi(maFourmiliere);
+	 Adulte monAdulte = new Adulte(maFourmi);
+	 monAdulte.setRole(this);
+
+	 maFourmi.setetatDev(monAdulte);
+	 setrefAdulte(monAdulte);
+	 
+	 maFourmiliere.putFourmi(maFourmi);
+	 
+	 refAdulte.setDureevieMax(dureevieReine);
+	 
+  }
+  
+  public void cycle(){
+	  
+	  if(refAdulte.getDureevie()%50==0)
+		  pondreoeuf();
+
+  }
+  
+  public void setrefAdulte(Adulte monAdulte) {
+	  refAdulte=monAdulte;
+  }
+  
+  public void pondreoeuf() {
+	 
+	 maFourmiliere.nouvelleFourmi();
+  }
+
+}
